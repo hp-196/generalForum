2cf74b1311db57b84566c5c3b6eb3dd8b13fc370
도넛맨 <hp196@naver.com>
2022-11-10 23:18:08 +0900

    중복 컬럼 추출

--- src/main/java/com/example/demo/domain/ArticleComment.java
+++ src/main/java/com/example/demo/domain/ArticleComment.java
@@ -20,7 +20,7 @@ import java.util.Objects;
         @Index(columnList = "createdAt")
 })
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,14 +28,6 @@ public class ArticleComment {
     @ManyToOne @Setter private Article article;
 
     @Setter @Column(nullable = false, length = 500) private String content; //본문
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
-
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
-
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
-
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
 
     protected ArticleComment() {
     }
